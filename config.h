5de5456da5ba6d5c8e4055b38f3eb067401c1b94
Patrick Neff <odie86@gmail.com>
2023-07-18 01:36:46 +0200

    Remove 5th tag

--- config.h
+++ config.h
@@ -31,7 +31,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "󰣇", "", "", "", "", };
+static const char *tags[] = { "󰣇", "", "", "", };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -105,7 +105,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
+	// TAGKEYS(                        XK_5,                      4)
 	// TAGKEYS(                        XK_6,                      5)
 	// TAGKEYS(                        XK_7,                      6)
 	// TAGKEYS(                        XK_8,                      7)
